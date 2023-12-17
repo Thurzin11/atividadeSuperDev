2a9646af9da35c7d5c44b011ad5795b38ddea3bb
Arthur <arthursantosgoncalves587@gmail.com>
2023-12-17 00:10:34 -0300

    adicionando funcoes na tela de login e na tela principal

--- script/main.js
+++ script/main.js
@@ -2,11 +2,18 @@
 const user = [
         {
                 login: 'arthur',
+                nome: 'Arthur Santos Goncalves',
                 senha: '2935'
         },
         {
                 login: 'jonathan',
-                senha: 'gostoso'
+                nome: 'Jonathan Silveira',
+                senha: '1999'
+        },
+        {
+                login: 'pedro',
+                nome: 'Pedro Artur Furtado',
+                senha: '1959'
         }
 ]
 
@@ -16,20 +23,48 @@ const login = document.querySelector('#login');
 const password = document.querySelector('#password');
 
 
-btnLogin.addEventListener('click', ()=>{
-        verificarUser();
+btnLogin.addEventListener('click', () => {
+        verificarUser(login,password);
 });
 
-function verificarUser(){
+function verificarUser(login,password) {
         let contador = 0;
         user.forEach((cont) => {
-                if(cont.login == login.value && cont.senha == password.value){
+                if (cont.login == login.value && cont.senha == password.value) {
                         alert('Login feito');
-                        window.location.href = "main.html"
+                        window.location.href = "main.html";
+
                         contador++;
                 }
         });
-        if(contador == 0){
+        if (contador == 0) {
                 alert('O login falhou')
         }
 }
+
+let inputPass = document.getElementById('password');
+let btnShowPass = document.getElementById('btn-password');
+let btnClosePass = document.getElementById('btn-text');
+btnShowPass.addEventListener('click', () => {
+        showPassword();
+})
+btnClosePass.addEventListener('click', () => {
+        showPassword();
+})
+function showPassword() {
+
+        if (inputPass.type === 'password') {
+                inputPass.setAttribute('type', 'text');
+                btnShowPass.style.display = 'none';
+                btnClosePass.style.display = 'flex';
+        } else {
+                if (inputPass.type === 'text') {
+                        inputPass.setAttribute('type', 'password');
+                        btnShowPass.style.display = 'flex';
+                        btnClosePass.style.display = 'none';
+                }
+        }
+}
+
+
+
--- script/script_index.js
+++ script/script_index.js
@@ -6,20 +6,87 @@ const s2 = document.querySelector('.second');
 const s3 = document.querySelector('.third');
 
 
-btn1.addEventListener('click', ()=>{
-        s1.style.display = 'flex';
-        s2.style.display = 'none';
-        s3.style.display = 'none';
+btn1.addEventListener('click', () => {
+    s1.style.display = 'flex';
+    s2.style.display = 'none';
+    s3.style.display = 'none';
 });
 
-btn2.addEventListener('click', ()=>{
+btn2.addEventListener('click', () => {
     s1.style.display = 'none';
     s2.style.display = 'flex';
     s3.style.display = 'none';
 });
 
-btn3.addEventListener('click', ()=>{
-        s2.style.display = 'none';
-        s1.style.display = 'none';
-        s3.style.display = 'flex';
+btn3.addEventListener('click', () => {
+    s2.style.display = 'none';
+    s1.style.display = 'none';
+    s3.style.display = 'flex';
 });
+
+function somar(a, b) {
+    const result = a + b;
+    return result;
+}
+
+function subtrair(a, b) {
+    const result = a - b;
+    return result;
+}
+
+function multiplicar(a, b) {
+    const result = a * b;
+    return result;
+}
+
+function dividir(a, b) {
+    const result = a / b;
+    return result;
+}
+
+// Função Callback recebendo por referência a operação matemática que será 
+// executada, e passando por parâmetro os valores dos inputs
+const result = document.querySelector('#resultado');
+function executarOperacao(operacao) {
+    const numA = parseInt(document.getElementById('numA').value);
+    const numB = parseInt(document.getElementById('numB').value);
+
+    const resultadoFinal = operacao(numA, numB);
+
+
+    result.innerHTML = `O resultado da operação é ${resultadoFinal}`;
+}
+
+
+const btnLogout = document.querySelector('#logout');
+btnLogout.addEventListener('click', () => {
+    sair();
+});
+
+function sair() {
+    window.location.href = "index.html"
+}
+
+const quantidadeAsterisco = document.querySelector('#pergunta-asterisco');
+const bntPiramide = document.querySelector('#gerarPiramide');
+const resultDois = document.querySelector('#resultadoDois');
+
+bntPiramide.addEventListener('click', () => {
+    gerarPiramide();
+});
+
+function gerarPiramide() {
+    resultDois.innerHTML = ``;
+    if(quantidadeAsterisco.value>0){
+        resultDois.style.background = '#2F6306';
+    for (let i = 0; i < quantidadeAsterisco.value; i++) {
+        for (let c = 0; c <= i; c++) {
+            resultDois.innerHTML += `*`;
+        }
+        resultDois.innerHTML += `<br>`
+    }
+}else{
+    resultDois.innerHTML += `Informe um valor valido`;
+    resultDois.style.background = 'red';
+}
+}
